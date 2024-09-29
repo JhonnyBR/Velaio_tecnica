b6346284efd2f5273fe1c0334492ee77925f37f9
Jonny Beltran <jhonybel17@gmail.com>
2024-09-29 13:12:13 -0500

    Se establece componente Home y se instalan dependecias globales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+//dependencias
+
+import { NgxSpinnerComponent, NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageModule } from 'primeng/image';
+import { DividerModule } from 'primeng/divider';
+import { MenubarComponent } from './menubar/menubar.component';
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerComponent,
+    BrowserAnimationsModule,
+    ImageModule,
+    DividerModule,
+    MenubarModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menubar/menubar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menubar',
+  templateUrl: './menubar.component.html',
+  styleUrls: ['./menubar.component.scss']
+})
+export class MenubarComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home'
+      }, 
+      {
+        label: 'Tareas',
+        icon: 'pi pi-fw pi-list',
+        items: [
+          {
+            label: "Crear",
+            icon: 'pi pi-fw pi-check'
+          }
+        ]
+      }
+    ]
+  }
+}
